56e86411789d2a51415ccf1d0f9dd74582240fe0
Certe Kim <seelkadoow@live.com>
2019-02-12 18:47:29 +0800

    version 0.0.1 updates:     mcmod.info Created

---
+++ src/main/java/cn/certekim/pedmod/EndlessDreamMain.java
@@ -0,0 +1,53 @@
+package cn.certekim.pedmod;
+
+import cn.certekim.pedmod.proxy.CommonProxy;
+import cn.certekim.pedmod.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**********************************************************************************************************************
+ * -------------------------------------------------------------------------------------------------------------------
+ *                                             Endless Dream Mod
+ *                                                              - Certe Kim
+ * -------------------------------------------------------------------------------------------------------------------
+ * Project Endless Dream.
+ *      The first mod created bt Certe Kim,for Minecraft.
+ *
+ * Version Alpha 0.0.1
+ *      Update Log:
+ *          Display modinfo
+ **********************************************************************************************************************/
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+public class EndlessDreamMain {
+
+    @Instance
+    public static EndlessDreamMain instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
---
+++ src/main/java/cn/certekim/pedmod/util/Reference.java
@@ -0,0 +1,10 @@
+package cn.certekim.pedmod.util;
+
+public class Reference {
+    public static final String MOD_ID = "ed";
+    public static final String NAME = "Endless Dream";
+    public static final String VERSION = "0.0.1";
+    public static final String ACCEPTED_VERSION = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "cn.certekim.pedmod.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "cn.certekim.pedmod.proxy.CommonProxy";
+}
