684b9215e8fe11d02490bb5d5bf07e5fe58d633f
Certe Kim <seelkadoow@live.com>
2019-02-12 22:20:16 +0800

    version 0.0.1 updates:     mcmod.info Created     CreativeTab Created

--- src/main/java/cn/certekim/pedmod/EndlessDreamMain.java
+++ src/main/java/cn/certekim/pedmod/EndlessDreamMain.java
@@ -1,8 +1,10 @@
 package cn.certekim.pedmod;
 
+import cn.certekim.pedmod.item.CreativeTabEndlessDreamMod;
 import cn.certekim.pedmod.proxy.CommonProxy;
 import cn.certekim.pedmod.util.Reference;
 
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -10,6 +12,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
 
 /**********************************************************************************************************************
  * -------------------------------------------------------------------------------------------------------------------
@@ -33,21 +36,26 @@ public class EndlessDreamMain {
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
     public static CommonProxy proxy;
 
+    public static Logger logger;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-
+        logger = event.getModLog();
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        proxy.init(event);
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        proxy.postInit(event);
     }
+
+    public static final CreativeTabs tabEndlessDream = new CreativeTabEndlessDreamMod("Endless Dream");
 }
